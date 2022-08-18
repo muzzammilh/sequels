e37183f119e64cb30ad0bb63e7d77c530fec66f6
HassamAli <hassamali@A006-00290.local>
2022-08-18 14:47:11 +0500

    add css into the app

--- src/App.js
+++ src/App.js
@@ -6,6 +6,19 @@ function App() {
   return (
     <>
     <Router>
+      <div className="flex justify-center py-8">
+        <ul className="flex w-1/2 justify-between">
+          <li >
+            <Link to="/">Home</Link>
+          </li>
+          <li >
+            <Link to="/trending">Trending</Link>
+          </li>
+          <li >
+            <Link to="/topics">Topics</Link>
+          </li>
+        </ul>
+      </div>
       <Switch>
         <Route path="/" exact>
           <Home />
@@ -14,19 +27,7 @@ function App() {
           <Trending />
         </Route>
       </Switch>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/trending">Trending</Link>
-          </li>
-          <li>
-            <Link to="/topics">Topics</Link>
-          </li>
-        </ul>
-        </div>
+      
     </Router>
     </>
   );
