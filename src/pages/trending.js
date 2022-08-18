e2794600b0cd053b0fc842b7216f89e48d83b4c6
HassamAli <hassamali@A006-00290.local>
2022-08-18 18:14:00 +0500

    test workflow

--- src/pages/trending.js
+++ src/pages/trending.js
@@ -1,6 +1,11 @@
 function Trending() {
     return (
+      <>
       <h1 className="text-7xl">Trending Page</h1>
+      <div className="w-2/6 h-1/2 bg-gray-200">
+
+      </div>
+      </>
     );
   }
   
