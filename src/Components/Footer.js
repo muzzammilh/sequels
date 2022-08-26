b7e2bfa9e8d9f83ddddac306b55e2cbbf9eb8250
HassamAli <hassamali@A006-00290.local>
2022-08-26 14:41:31 +0500

    new feature add

---
+++ src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="w-full h-20 bg-gray-800 flex justify-center items-center">
+      <h1 className="text-white text-xl">Footer</h1>
+    </div>
+  );
+};
+
+export default Footer;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,22 +1,17 @@
 import React from 'react';
-import {useSelector, useDispatch} from 'react-redux';
-import {ActionType} from '../Redux/Contants/action-type';
+import Footer from '../Components/footer';
+// import {useSelector, useDispatch} from 'react-redux';
+// import {ActionType} from '../Redux/Contants/action-type';
 
 
 const Home = () => {
-  const {count} = useSelector((state) => state.counter);
-  const dispatch = useDispatch();
+  // const {count} = useSelector((state) => state.counter);
+  // const dispatch = useDispatch();
   return (
     <div
       className='w-screen h-80 flex justify-center items-center flex-col gap-8'>
-      <h1>{count}</h1>
-      <div className='flex gap-2'>
-        <button onClick={()=>{
-          dispatch({type: ActionType.INCREMENT});
-        }}>Increment</button>
-        <button onClick={()=>{
-          dispatch({type: ActionType.DECREMENT});
-        }}>Decrement</button>
+      <div>
+        <Footer/>
       </div>
     </div>
   );
