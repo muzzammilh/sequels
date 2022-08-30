5144a69de50f8e1b564fdd88b7d1c051653b4b95
HassamAli <hassamali@A006-00290.local>
2022-08-30 16:49:54 +0500

    bug ifx

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,28 +1,9 @@
 import React from 'react';
-import TabBar from '../Components/NavBars/TabBar';
-import VerticalCard from '../Components/Cards/VerticalCard';
 
 const Home = () => {
   return (
     <div>
-      <div>
-        <h1 className='text-xl font-bold'>Whats Popular</h1>
-      </div>
-      <div className='py-4'>
-        <TabBar tabList={['Streeming', 'ON TV', 'For Rent', 'In Theaters']}/>
-      </div>
-      <div className=''>
-        {
-          [1, 2, 3, 4].map((item, index) => {
-            return (
-            // eslint-disable-next-line react/jsx-key
-              <div>
-                <VerticalCard/>
-              </div>
-            );
-          })
-        }
-      </div>
+      Home
     </div>
   );
 };
