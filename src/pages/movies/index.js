3b0180bd925c1a98851ca9fdf773b338516df66b
UraizAli <uraiz.ali@arbisoft.com>
2022-09-08 23:26:28 +0500

    design movie page

--- src/pages/movies/index.js
+++ src/pages/movies/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import TabBar from '../../Components/NavBars/TabBar';
+import VerticalCard from '../../Components/Cards/VerticalCard';
 
 const Movies = () => {
   return (
-    <div>
-            Moives
-    </div>
+    <>
+      <div>
+        <h1 className='font-bold'>Movies</h1>
+      </div>
+      <div>
+        <TabBar tabList={['Popular', 'Now Playing', 'Upcoming', 'Top Rated']} />
+      </div>
+      <div className='flex flex-wrap py-2 gap-2'>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((item, index) => {
+          return <div key={item}>
+            <VerticalCard/>
+          </div>;
+        })
+        }
+      </div>
+
+    </>
   );
 };
 
