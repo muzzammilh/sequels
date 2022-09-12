a4261719af29c2b1f0e0fffa7975d91a4028725d
HassamAli <hassamali@A006-00290.local>
2022-09-12 15:26:15 +0500

    added tv-shows page content

--- src/Components/NavBars/TabBar.js
+++ src/Components/NavBars/TabBar.js
@@ -13,7 +13,7 @@ const TabBar = ({className, tabList, onClick, fontSize}) => {
               <button className={[' capitalize font-bold ',
                 className]}
               style={{color: activeTab==index ? colors.primary :
-                colors.grayDark, fontSize: fontSize || '0.8rem'}}
+                colors.grayDark, fontSize: fontSize}}
               onClick={() => {
                 activeTab!=index && setActiveTab(index);
                 onClick && onClick(index);
