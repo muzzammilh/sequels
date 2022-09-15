52e8e235920df6ed49637f4ee1210028fb7cee03
Hassam Ali <73829447+hassamali898@users.noreply.github.com>
2022-09-15 14:55:55 +0500

    Added Content in Tv shows details page (#49)
    * created all pages of the TMDB project
    
    * bug fixes
    
    * fix and upgrade tailwindcss and responsive issues
    
    * add some features
    
    * bug ifx
    
    * changing file name to uppercase
    
    * movie-details page created and also added some features
    
    * convert the detail section into component for future use into different files and also added casts
    
    * bug fix
    
    * completed details page and also added new features
    
    * added content in tv-shows details page
    
    * fixed package-lock problem
    
    Co-authored-by: HassamAli <hassamali@A006-00290.local>
    Co-authored-by: HassamAli <hassamali@192.168.10.5>

--- src/pages/tv-shows/details.js
+++ src/pages/tv-shows/details.js
@@ -1,9 +1,63 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Casts from '../../Components/Details/Casts';
+// eslint-disable-next-line max-len
+import ContentAndContributors from '../../Components/Details/ContentAndContributors';
+import ExternalID from '../../Components/Details/ExternalID';
+import Head from '../../Components/Details/Head';
+import Keywords from '../../Components/Details/Keywords';
+import Media from '../../Components/Details/Media';
+import Recomendation from '../../Components/Details/Recomendation';
+import Sessions from '../../Components/Details/Session';
+import Social from '../../Components/Details/Social';
+import {colors} from '../../theme/colors';
 
 const TvShowDetails = () => {
+  const [vibrant, setVibrant] = useState(null);
+  const forgorundColor = (parseInt(vibrant?.replace('#', ''), 16) >
+  0xffffff / 2)? colors.primaryLight : colors.primary;
   return (
     <div>
-            Tv Show Details
+      <Head vibrant={vibrant} setVibrant={setVibrant}
+        forgorundColor={forgorundColor}/>
+      <div className='flex flex-wrap'>
+        <div className='w-full md:w-4/6 mt-10 md:mb-10'>
+          <Casts/>
+          <Sessions/>
+          <Social/>
+          <Media/>
+          <Recomendation/>
+        </div>
+        <div className='w-full md:w-2/6 my-10 md:px-10'>
+          <ExternalID/>
+          <div className='my-6'>
+            <div className='py-2'>
+              <h2 className='font-semibold'>Fact</h2>
+              {/* <h2 className='font-light leading-4 text-sm'>Released</h2> */}
+            </div>
+            <div className='py-2'>
+              <h2 className='font-semibold'>Original Name</h2>
+              <h2 className='font-light leading-4 text-sm'>
+                S.O.Z: Soldados o Zonmbies</h2>
+            </div>
+            <div className='py-2'>
+              <h2 className='font-semibold'>Status</h2>
+              <img src='https://www.themoviedb.org/t/p/h60/ifhbNuuVnlwYy5oXA5VIb2YR8AZ.svg'
+                className='h-8 mt-2'/>
+            </div>
+            <div className='py-2'>
+              <h2 className='font-semibold'>Type</h2>
+              <h2 className='font-light leading-4 text-sm'>Scripted</h2>
+            </div>
+            <div className='py-2'>
+              <h2 className='font-semibold'>Original Language</h2>
+              <h2 className='font-light leading-4 text-sm'>English</h2>
+            </div>
+          </div>
+          <Keywords/>
+          <ContentAndContributors vibrant={vibrant}
+            forgroundColor={forgorundColor}/>
+        </div>
+      </div>
     </div>
   );
 };
